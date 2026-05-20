11a3a2c3754d6a1a4eb6248be75a8dcc1bca64b8
vesselvi <ignacio.caram.43@gmail.com>
2026-05-19 22:31:31 -0300

    added the dropdown funciontality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,57 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.11.0/+esm';
 
-const response = await axios.get('http://localhost:3000/tasks');
-console.log(response.data);
+
+const getTasks = async () => {
+
+    try {
+
+        const response = await axios.get('http://localhost:3000/tasks');
+
+        console.log(response.data);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+};
+
+getTasks();
+
+
+const dropdownItems = document.querySelectorAll(".dropdown-item");
+
+dropdownItems.forEach(item => {
+
+    item.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const selectedText = event.target.textContent.trim();
+
+        const dropdown = event.target.closest(".dropdown");
+
+        const button = dropdown.querySelector(".statusBtn");
+
+        button.textContent = selectedText;
+
+        button.classList.remove("btn-secondary",
+            "btn-danger",
+            "btn-success");
+
+        if (selectedText === "Pendiente") {
+            button.classList.add("btn-secondary");
+        }
+
+        else if (selectedText === "En progreso") {
+            button.classList.add("btn-danger");
+        }
+
+        else if (selectedText === "Completado") {
+            button.classList.add("btn-success");
+        }
+
+
+    })
+})
+
